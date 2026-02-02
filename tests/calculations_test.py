68327167e4e3e8e1a6320f335015b473b75ec3d8
yousiftriki <152455699+yousiftriki@users.noreply.github.com>
2026-02-02 00:20:32 +0000

    Update calculations_test.py

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -57,14 +57,13 @@ def test_get_nth_fibonacci_one():
     # Assert
     assert result == 1
 
+def test_get_nth_fibonacci_ten():
+     """Test with n=10."""
+     # Arrange
+     n = 10
 
-# def test_get_nth_fibonacci_ten():
-#     """Test with n=10."""
-#     # Arrange
-#     n = 10
+     # Act
+     result = get_nth_fibonacci(n)
 
-#     # Act
-#     result = get_nth_fibonacci(n)
-
-#     # Assert
-#     assert result == 89
+     # Assert
+     assert result == 89
